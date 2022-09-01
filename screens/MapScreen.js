5902f2344d9c82801a401ad64959510513508e0c
trrrbumba <badaro@list.ru>
2022-09-02 00:48:16 +0300

    first commit

---
+++ screens/MapScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const MapScreen = () => {
+    return (
+        <View>
+            <Text>Here is map stuff...</Text>
+        </View>
+    )
+}
+
+export default MapScreen;
