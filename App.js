9415834193352ae981c0156b1a585b9dd535d042
trrrbumba <badaro@list.ru>
2022-09-03 22:18:53 +0300

    second vertion

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
 import { store } from "./store";
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stact = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stact.Navigator>
+            <Stact.Screen
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+
+            <Stact.Screen
+              name='MapScreen'
+              component={MapScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+          </Stact.Navigator>
+          {/* <HomeScreen /> */}
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
